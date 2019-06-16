45a6a8206d09ebaabbd4a5b8e76237c721f1ac93
arman-sh <rosko1245@gmail.com>
2019-06-16 21:55:05 +0300

    def

--- labs/lab5/lab5_4/lab5_4.sv
+++ labs/lab5/lab5_4/lab5_4.sv
@@ -11,20 +11,24 @@ module lab5_4
 	reg [N-1:0] rg_int;
 	reg [N-1:0] temp;
 	
-	always @*
-	if(sel==2'b00) 
-		temp = da;
-	else if(sel == 2'b01) 
-		temp = dc;
-	else if(sel == 2'b10) 
-		temp = db;
+	always @* begin
+		if(sel==2'b00) 
+			temp = da;
+		else if(sel == 2'b01) 
+			temp = dc;
+		else if(sel == 2'b10) 
+			temp = db;
 		else temp = 0;
+	end
 	
-	always @(posedge clk, posedge load)
+	always @(posedge clk)
+	begin
 		if(load)
 			rg_int <= temp;
 		else 
 			rg_int<= {rg_int[N-2:0], rg_int[N-1]};
+	end
+	
 	assign q_out = rg_int;
 
 endmodule	
